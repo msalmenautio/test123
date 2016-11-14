b9ba0eafa332bc706b11871923fe8ef9b032d76f
Marko S <Marko S>
2016-11-14 16:00:39 +0200

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const socketIO = require('socket.io');
+const app = express();
+var client = require('heroku-redis').createClient(process.env.REDIS_URL);
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/softphone.html')
-})
+const PORT = process.env.PORT ||8080;
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+
+
+const server = express()
+  .use((req, res) =>  res.sendFile(__dirname + '/softphone.html')) 
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('make_a_call', function (data) {
+	 console.log(data); });
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+
+
